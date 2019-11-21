42209fac648c5355c8453889fabdaecd1ed7dbb0
CeriseDW <carnetsdecerise@gmail.com>
2019-11-21 14:33:26 +0100

    first commit

---
+++ NetFlask.DAL/MovieRepository.cs
@@ -0,0 +1,106 @@
+﻿using NetFlask.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToolBox.Database;
+namespace NetFlask.DAL
+{
+    public class MovieRepository
+    {
+        static Random rnd = new Random();
+        private Connection _oconn;
+        private string _cnstr = @"Data Source=WAD-12\ADMINSQL;Initial Catalog=NetFlask;User ID=aspuser;Password=test1234=;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public MovieRepository()
+        {
+            _oconn = new Connection(_cnstr);
+        }
+
+        public List<string> getFirstSix()
+        {
+            IEnumerable<string> images = new List<string>();
+            Command cmd = new Command("SELECT TOP 6 Picture FROM MOVIE");
+            images =_oconn.ExecuteReader<string>(cmd, d=>d["Picture"].ToString());
+
+            return images.ToList();
+        }
+
+        public List<string> getFromTitle(string search)
+        {
+            IEnumerable<string> images = new List<string>();
+            Command cmd = new Command($"SELECT * FROM MOVIE where title like '%{search}%'");
+            images = _oconn.ExecuteReader<string>(cmd, d => d["Picture"].ToString());
+
+            return images.ToList();
+        }
+
+        public List<Movie> getMoviesFromTitle(string search)
+        {
+            IEnumerable<Movie> movies = new List<Movie>();
+            Command cmd = new Command($"SELECT * FROM Movie WHERE Title LIKE '%{search}%'");
+            movies = _oconn.ExecuteReader(cmd,
+                d => new Movie()
+                {
+                    IdMovie = (int)d["IdMovie"],
+                    Picture = d["Picture"].ToString(),
+                    Title = d["Title"].ToString(),
+                    Duration = (int)d["Duration"],
+                    Summary = d["Summary"].ToString(),
+                    ReleaseDate = (DateTime)d["ReleaseDate"],
+                    // TODO : Mapping des autres colonnes
+                }
+                );
+            return movies.ToList();
+        }
+
+        public List<string> getRandom()
+        {
+            int skip = rnd.Next(0, 990);
+
+            IEnumerable<string> images = new List<string>();
+            Command cmd = new Command($"SELECT   Picture FROM MOVIE order by IdMovie  OFFSET ({skip}) ROWS FETCH NEXT (6) ROWS ONLY");
+            images = _oconn.ExecuteReader<string>(cmd, d => d["Picture"].ToString());
+
+            return images.ToList();
+        }
+
+        public List<Movie> getTrailer()
+        {
+            IEnumerable<Movie> movies = new List<Movie>();
+            Command cmd = new Command($"SELECT TOP 50 * FROM Movie");
+            movies = _oconn.ExecuteReader(cmd,
+                d=> new Movie()
+                {
+                    IdMovie = (int)d["IdMovie"],
+                    Picture = d["Picture"].ToString(),
+                    Title = d["Title"].ToString(),
+                    Duration = (int)d["Duration"],
+                    Summary = d["Summary"].ToString(),
+                    ReleaseDate = (DateTime)d["ReleaseDate"],
+                    Trailer = d["Trailer"].ToString()
+                });
+            return movies.ToList();
+        }
+
+        public List<Movie> getTrailerPagination(int ipage)
+        {
+            IEnumerable<Movie> movies = new List<Movie>();
+            Command cmd = new Command($"SELECT * FROM Movie ORDER BY IdMovie  OFFSET {50 * (ipage - 1)} ROWS FETCH NEXT (50) ROWS ONLY");
+            movies = _oconn.ExecuteReader(cmd,
+                d => new Movie()
+                {
+                    IdMovie = (int)d["IdMovie"],
+                    Picture = d["Picture"].ToString(),
+                    Title = d["Title"].ToString(),
+                    Duration = (int)d["Duration"],
+                    Summary = d["Summary"].ToString(),
+                    ReleaseDate = (DateTime)d["ReleaseDate"],
+                    Trailer = d["Trailer"].ToString()
+                }
+                );
+            return movies.ToList();
+        }
+
+    }
+}
---
+++ NetFlask/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using NetFlask.DAL;
+using NetFlask.Entities;
+using NetFlask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NetFlask.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            ViewBag.Current = "Index";            
+
+            return View(new HomeViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult SimpleSearch(string txtSearch)
+        {
+            ViewBag.SearchString = txtSearch;
+
+            MovieRepository rm = new MovieRepository();
+            List<Movie> liste = rm.getMoviesFromTitle(txtSearch);
+            List<MovieReview> listeView = new List<MovieReview>();
+            foreach (Movie movie in liste)
+            {
+                listeView.Add(new MovieReview()
+                {
+                    Picture = movie.Picture,
+                    Title = movie.Title,
+                    Duration = movie.Duration,
+                    Summary = movie.Summary,
+                    Release = movie.ReleaseDate,
+                }
+                );
+            }
+
+            return View("resultat",listeView);
+        }
+
+
+
+        public ActionResult Videos(int id = 1)
+        {
+            ViewBag.Current = "Videos";
+            ViewBag.NextPageNmb = id + 1;
+            MovieRepository mr = new MovieRepository();
+            List<Movie> liste = mr.getTrailerPagination(id);
+
+            List<MovieInfo> listeView = new List<MovieInfo>();
+            foreach (Movie movie in liste)
+            {
+                listeView.Add(new MovieInfo()
+                {
+                    IdMovie = movie.IdMovie,
+                    Picture = movie.Picture,
+                    Title = movie.Title,
+                    Summary = movie.Summary,
+                    Release = movie.ReleaseDate.Value,
+                    Video = (movie.Trailer).Replace("watch?v=", "embed/"),
+                }
+                );
+            }
+
+
+            return View(listeView);
+        }
+
+        public ViewResult Reviews()
+        {
+            ViewBag.Current = "Reviews";
+            List<MovieReview> movrev = new List<MovieReview>();
+            movrev = FakeDb.LoadData();
+            //string CastString = convertToString(movrev.Cast);
+            //ViewBag.CastString = CastString;
+            //BERK BERK BERK BERK!!!!!!!!!!!!!! 
+            return View(movrev);
+        }
+
+        private string convertToString(List<string> casts)
+        {if (casts.Count() == 0) return "";
+            string retour = "";
+            foreach (string item in casts)
+            {
+                retour += item + ",";
+
+            }
+            return retour.Substring(0, retour.Length - 1);
+        }
+    }
+}
